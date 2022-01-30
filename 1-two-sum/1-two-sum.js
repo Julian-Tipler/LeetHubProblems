ab2879506644e00f67a0f161f5c7f16e1c3da159
Julian Tipler <tipler.julian@gmail.com>
2022-01-30 10:51:28 -0600

    Time: 103 ms (67.79%), Space: 45 MB (5.12%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,12 +4,17 @@
  * @return {number[]}
  */
 const twoSum = (nums, target)=> {
+    let output = []
     let map = new Map()
     for(let i = 0;i<nums.length;i++) {
         let num = nums[i]
-        if(map.has(target-num)) {
-            return [i,map.get(target-num)]
+        let counterpart = target-num
+        if(map.has(counterpart)) {
+            output.push(map.get(counterpart),i)
         }
-        else map.set(num,i)
+        else {
+            map.set(num,i)
+        }
     }
+    return output
 };
