1bcf6019fd5e88c68c9d30d5c81b7828289246bc
Julian Tipler <tipler.julian@gmail.com>
2022-03-24 20:38:56 -0500

    Time: 245 ms (58.89%), Space: 49.9 MB (91.62%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,7 +3,13 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    const strNum = x.toString()
-    if(strNum.split("").reverse().join("") === strNum) return true
-    return false
+    let original = x
+    //one digit number
+    if(x<10 && x>=0) return true
+    let reversed = 0
+    while(original>0) {
+        reversed = reversed*10 + original%10
+        original = (original - original%10)/10
+    }
+    return reversed === x
 };
