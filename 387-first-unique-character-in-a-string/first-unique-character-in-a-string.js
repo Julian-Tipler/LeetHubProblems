ff0dd0f89e84b3367bcd12efde65dbbe00603b12
Julian Tipler <tipler.julian@gmail.com>
2024-02-04 22:27:38 -0600

    Time: 88 ms (60.64%) | Memory: 52.1 MB (29.70%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -2,17 +2,8 @@
  * @param {string} s
  * @return {number}
  */
-var firstUniqChar = function (s) {
-    const dict = {}
-    for (let i = 0; i < s.length; i++) {
-        dict[s[i]] !== undefined ? dict[s[i]] = -1 : dict[s[i]] = i
-    }
-    console.log(dict)
-    let index = Number.MAX_VALUE
-    Object.keys(dict).forEach((key) => {
-        if (dict[key] !== -1) {
-            index = Math.min(index, dict[key])
-        }
-    })
-    return index === Number.MAX_VALUE ? -1 : index
+var firstUniqChar = function(s) {
+    for(i=0; i<s.length; i++)
+        if(s.indexOf(s[i])===s.lastIndexOf(s[i])) return i
+    return -1
 };
