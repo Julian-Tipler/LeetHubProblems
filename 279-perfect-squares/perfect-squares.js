08d8bc73a8bdb13d60c8e387612dd3eaac8b2add
Julian Tipler <tipler.julian@gmail.com>
2024-02-09 12:27:32 -0600

    Time: 259 ms (28.30%) | Memory: 54.9 MB (22.25%) - LeetSync

--- 279-perfect-squares/perfect-squares.js
+++ 279-perfect-squares/perfect-squares.js
@@ -3,51 +3,26 @@
  * @return {number}
  */
 
-// var numSquares = function (n) {
-//     const memo = {}
-
-//     const squares = []
-//     let val = 1
-//     while (val * val <= n) {
-//         squares.push(val * val)
-//         val++
-//     }
-//     let output = Number.MAX_VALUE
-//     const dfs = (sum, index, count) => {
-//         memo[squares[index] = Math.min(squares[index], count)]
-//         if (sum > n) return false
-//         if (sum === n) {
-//             output = Math.min(count, output)
-//             return true
-//         }
-//         for (let i = index; i >= 0; i--) {
-//             dfs(sum + squares[i], i, count + 1)
-//         }
-//     }
-//     dfs(0, squares.length - 1, 0)
-//     return output
-// };
 var numSquares = function (n) {
-    // returns the min number of squares to hit the sum
+    // keeps track of minimum count for each sum
     let memo = {}
     const dfs = (sum) => {
+        // if the dfs for this sum exists, return the count
         if (memo[sum]) return memo[sum]
+        // if the sum is now 0 return a count of 0
         if (sum === 0) return 0
-        if (sum < 0) return false
+
         let min = Number.MAX_VALUE
+        // iterate through each of the squares (less than current sum)
         for (let i = 1; i * i <= sum; i++) {
-            let square = i * i;
-            const count = dfs(sum - square) + 1
-            min = Math.min(count, min)
+            let square = i * i
+            // count is equal to the dfs of the sum - current square (which may be memoized)
+            let count = dfs(sum - square) + 1
+            min = Math.min(min, count)
         }
+        // at this point the min is the minimum count for the current sum, and can be memoized
         memo[sum] = min
         return min
     }
     return dfs(n)
 }
-
-
-// naive solution
-// dfs through with backtracking until many combinations of squares are tried
-
-// maybe most efficient solution will use memoization?
