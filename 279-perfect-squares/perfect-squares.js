d3d0b5ac00dfa925d9d646d6f628ba18fa084c71
Julian Tipler <tipler.julian@gmail.com>
2024-02-11 13:23:49 -0600

    Time: 1144 ms (5.07%) | Memory: 55.9 MB (14.79%) - LeetSync

--- 279-perfect-squares/perfect-squares.js
+++ 279-perfect-squares/perfect-squares.js
@@ -4,23 +4,19 @@
  */
 
 var numSquares = function (n) {
-    // keeps track of minimum count for each sum
     let memo = {}
     const dfs = (sum) => {
-        // if the dfs for this sum exists, return the count
         if (memo[sum]) return memo[sum]
-        // if the sum is now 0 return a count of 0
         if (sum === 0) return 0
-
+        if (sum < 0) return false
         let min = Number.MAX_VALUE
-        // iterate through each of the squares (less than current sum)
-        for (let i = 1; i * i <= sum; i++) {
+        for (let i = 1; i * i <= n; i++) {
             let square = i * i
-            // count is equal to the dfs of the sum - current square (which may be memoized)
-            let count = dfs(sum - square) + 1
-            min = Math.min(min, count)
+            let success = dfs(sum - square)
+            if (success !== false) {
+                min = Math.min(min, success + 1)
+            }
         }
-        // at this point the min is the minimum count for the current sum, and can be memoized
         memo[sum] = min
         return min
     }
