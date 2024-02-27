f1f834530e0e0ce895479d4b1f529c77f8e9ef1c
Julian Tipler <tipler.julian@gmail.com>
2024-02-27 10:23:14 -0600

    Time: 56 ms (100.00%) | Memory: 54.9 MB (23.81%) - LeetSync

---
+++ 623-add-one-row-to-tree/add-one-row-to-tree.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @param {number} depth
+ * @return {TreeNode}
+ */
+var addOneRow = function (root, val, depth) {
+    // traverse dfs style and stop once depth - 1 is reached
+    // At this node do node.left = new node and new node.left = node.left
+
+    let mockNode = new TreeNode(null, root, null)
+    const dfs = (node, currDepth) => {
+        if (currDepth === depth - 1) {
+            let newLeft = new TreeNode(val, node.left, null)
+            let newRight = new TreeNode(val, null, node.right)
+            node.left = newLeft
+            node.right = newRight
+            return node
+        }
+        node.left && dfs(node.left, currDepth + 1)
+        node.right && dfs(node.right, currDepth + 1)
+        return node
+    }
+    dfs(mockNode, 0)
+    return mockNode.left
+};
+
+// depth can be === 1
