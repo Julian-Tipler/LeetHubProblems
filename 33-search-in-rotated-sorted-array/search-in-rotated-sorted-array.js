a411db44aa5f8675f776640b3f0487dea47f5930
Julian Tipler <tipler.julian@gmail.com>
2024-03-06 10:46:49 -0600

    Time: 50 ms (71.47%) | Memory: 48.9 MB (40.80%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let start = 0
+    let end = nums.length - 1
+    while (start<=end) {
+        mid = Math.floor((end + start) / 2)
+        if (nums[mid] === target) return mid
+        // left sorted
+        if (nums[start] <= nums[mid]) {
+            if (nums[start] <= target && target < nums[mid]) {
+                end = mid - 1
+            } else {
+                start = mid + 1
+            }
+        }
+        //right sorted
+        else {
+            if (nums[mid] < target && target <= nums[end]) {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        }
+    }
+    return -1
+};
+
+[3, 4, 5, 6, 7, 1, 2]
