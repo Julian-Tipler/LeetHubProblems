d209ac2f1db8cfa32357e81159aec693593d56b4
Julian Tipler <tipler.julian@gmail.com>
2024-03-07 10:31:45 -0600

    Time: 818 ms (5.77%) | Memory: 59 MB (13.46%) - LeetSync

---
+++ 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numSplits = function (s) {
+    let goodSplits = 0
+    let leftMap = new Map()
+    let rightMap = new Map()
+
+    leftMap.set(s[0], 1)
+    let i = 1
+    while (i < s.length) {
+        let curr = rightMap.get(s[i])
+        rightMap.set(s[i], curr ? curr + 1 : 1)
+        i++
+    }
+
+    i = 1
+    while (i < s.length) {
+        console.log(leftMap.length,rightMap.length)
+        if (leftMap.size === rightMap.size) {
+            goodSplits++
+        }
+        leftMap.set(s[i], 1)
+        if (rightMap.get(s[i]) === 1) {
+            rightMap.delete(s[i])
+        }
+        else rightMap.set(s[i], rightMap.get(s[i]) - 1)
+        i++
+    }
+    return goodSplits
+};
