770eb39a2c91668b346a2178f14e441deb698051
Julian Tipler <tipler.julian@gmail.com>
2024-03-16 13:51:04 -0500

    Time: 112 ms (30.95%) | Memory: 59.5 MB (16.45%) - LeetSync

--- 525-contiguous-array/contiguous-array.js
+++ 525-contiguous-array/contiguous-array.js
@@ -6,20 +6,16 @@ var findMaxLength = function (nums) {
     let count = 0
 
     let champ = 0
-    let map = {0:0}
-    for (let i = 0; i <= nums.length; i++) {
-
-        // if the count has already been hit
-        if (count in map) {
+    const map = { 0: -1 }
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] === 1 ? count++ : count--
+        if (count in map) {  // if the count has already been hit
             if (i - map[count] > champ) {
                 champ = i - map[count]
             }
-        }
-        // if this is the first time this count has been hit
-        else {
+        } else {  // if this is the first time this count has been hit
             map[count] = i
         }
-        nums[i] === 1 ? count++ : count--
     }
     return champ
 };
