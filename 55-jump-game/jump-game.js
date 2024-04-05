7992f207380fda6697bbbeb785f234c76382358b
Julian Tipler <tipler.julian@gmail.com>
2024-04-04 21:40:52 -0500

    Time: 68 ms (43.73%) | Memory: 52.8 MB (74.92%) - LeetSync

--- 55-jump-game/jump-game.js
+++ 55-jump-game/jump-game.js
@@ -2,14 +2,7 @@ var canJump = function (nums) {
     let maxIndex = 0
     for (let i = 0; i < nums.length; i++) {
         if (i > maxIndex) return false
-        let steps = nums[i]
-
-        maxIndex = Math.max(i + steps, maxIndex)
+        if (i + nums[i] > maxIndex) maxIndex = i + nums[i]
     }
     return true
 };
-
-// doesn't matter what jump length last index is
-// naive solution:
-// starting at the first index, dfs() in a for loop for the number of steps.
-// If the last index is reached eventually return true
