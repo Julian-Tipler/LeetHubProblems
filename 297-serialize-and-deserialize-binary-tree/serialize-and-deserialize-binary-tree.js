3a3ff7b30c4160e865b291a42c037f630ea2f2d1
Julian Tipler <tipler.julian@gmail.com>
2024-05-21 14:43:31 +0100

    Time: 131 ms (29.80%) | Memory: 62.3 MB (26.89%) - LeetSync

---
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.js
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function (root) {
+    const output = []
+    const dfs = (node) => {
+        //base case
+        if (node === null) {
+            output.push(null)
+            return
+        }
+        //preorder
+        output.push(node.val)
+        dfs(node.left)
+        dfs(node.right)
+    }
+    dfs(root)
+
+    return output
+};
+
+// [1, 2, null, null, 3, 4, null, null, 5, null, null]
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+
+// [1,null,null]
+// node = arr.shift()
+// This will shift values from the array leaving only the right tree
+// node.left = dfs()
+// By the time we hit the right tree we will only have the right tree values remaining
+// node.right = dfs()
+// would it be more effiecient to sort and use pop()?
+
+var deserialize = function (data) {
+
+    const dfs = (value) => {
+        if (value === null) {
+            return null
+        }
+        const left = dfs(data.shift())
+        const right = dfs(data.shift())
+        return new TreeNode(value, left, right)
+    }
+    return dfs(data.shift())
+};
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
