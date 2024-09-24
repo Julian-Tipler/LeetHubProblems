1965d0e1f5dc6f1caebfad0c04beab6b8456d49e
Julian Tipler <tipler.julian@gmail.com>
2024-09-24 12:23:46 -0500

    Time: 70 ms (11.88%) | Memory: 51.1 MB (52.87%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.js
+++ 605-can-place-flowers/can-place-flowers.js
@@ -9,7 +9,6 @@ var canPlaceFlowers = function (flowerbed, n) {
         const prev = flowerbed[i - 1] === undefined ? 0 : flowerbed[i - 1]
         const next = flowerbed[i + 1] === undefined ? 0 : flowerbed[i + 1]
         if (prev === 0 && next === 0 && flowerbed[i] === 0) {
-            console.log(i)
             i++
             n--
         }
